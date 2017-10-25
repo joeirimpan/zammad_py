a2d6450697e8f7eff27381ec216e823a116d997e
Joe Paul <joeirimpan@gmail.com>
2017-10-25 11:19:40 +0530

    Released version 0.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='zammad_py',
-    version='0.1.0',
+    version='0.1.1',
     description="Python API client for zammad",
     long_description=readme + '\n\n' + history,
     author="Joe Paul",
