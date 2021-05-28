8a4b6dc8a94909efd638f7b7e0f5196121b2304c
Joe Paul <joeirimpan@gmail.com>
2021-05-28 21:38:17 +0530

    chore: Bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='zammad_py',
-    version='0.1.7',
+    version='1.0.0',
     description="Python API client for zammad",
     long_description=readme + '\n\n' + history,
     author="Joe Paul",
--- zammad_py/__init__.py
+++ zammad_py/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Joe Paul"""
 __email__ = 'joeirimpan@gmail.com'
-__version__ = '0.1.7'
+__version__ = '1.0.0'
 
 from .api import ZammadAPI  # noqa
